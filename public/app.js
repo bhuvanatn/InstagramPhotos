f1f2a28299194c9e3add140dcc96bde9e4b4d5ba
Bhuvana <bhuvanatn@gmail.com>
2016-10-31 22:09:47 +1100

    bootstrap-lightbox added in controller

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('galPhoto',['ngRoute', 'ngResource']);
+var app = angular.module('galPhoto',['ngRoute', 'ngResource', 'bootstrapLightbox']);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
--- public/controllers/gallery.controller.js
+++ public/controllers/gallery.controller.js
@@ -1,23 +1,29 @@
 angular.module('galPhoto')
-.controller('GalleryCtrl',['$scope','instagram', function($scope, instagram){
+.controller('GalleryCtrl',['$scope','instagram', 'Lightbox', function($scope, instagram, Lightbox){
+	var instaArray =[];
 
 instagram.Profile()
   .then(function(resp){
-		console.log("I am in Profile");
-		console.log(resp);
 		$scope.fullname = resp.data.data.full_name;
 		$scope.profileimage = resp.data.data.profile_picture;
 	})
 	.catch(function(resp){
-		console.log(resp);
 	});
+
 instagram.AllImage()
     .then(function(resp){
-			console.log("I am in AllImage");
-			console.log(resp);
 			$scope.images = resp.data.data;
+
+			angular.forEach(resp.data.data, function(value, key){
+				instaArray.push(value.images.standard_resolution);
+			});
+
+			$scope.openLightboxModal = function(index){
+				Lightbox.openModal(instaArray, index);
+			};
 		})
 		.catch(function(resp){
-			console.log(resp);
 		});
+
+
 }]);
